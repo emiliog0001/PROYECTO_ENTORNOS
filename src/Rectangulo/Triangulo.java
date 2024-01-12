ae402a1fdd97b809b0fffcd31453c3ad009167bb
yajaira <yajaira@1DAM-23>
2024-01-12 13:43:58 +0100

    paso 1 listo!

--- src/Rectangulo/Triangulo.java
+++ src/Rectangulo/Triangulo.java
@@ -1,5 +1,35 @@
 package Rectangulo;
 
+import java.util.Scanner;
+
 public class Triangulo {
+
+	public static void main(String[] args) {		
+		
+		System.out.println("Introduce la base del rectángulo: ");
+		double base = introEntero();
+		System.out.println("Introduce la altura del rectángulo: ");
+		double altura = introEntero();
+		double resultado1 = calcularArearectangulo(base,altura);
+		verResultado(base,altura,resultado1);
+
+	}
+	
+	public static double introEntero() {
+		Scanner entrada = new Scanner(System.in);		
+		return entrada.nextDouble();
+	}
+	
+	public static double calcularArearectangulo(double base, double altura) {
+		
+		return base * altura;
+		
+	}
+	
+	public static void verResultado(double base,double altura,double resultado1) {
+		
+		System.out.println("El área del rectángulo es: "+ resultado1);
+		
+	}
 
 }
